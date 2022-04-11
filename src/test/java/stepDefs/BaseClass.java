654faf2b842889de45d7c829b143082570105922
shazanwer <shazanwer@gmail.com>
2022-04-11 08:59:44 +0300

    Updated Base Class

--- src/test/java/stepDefs/BaseClass.java
+++ src/test/java/stepDefs/BaseClass.java
@@ -9,15 +9,14 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 public class BaseClass {
-	
+
 	public static WebDriver driver;
-	
+
 	@Before
 	public void Setup() {
 
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		driver = new ChromeDriver();
-//		driver.get("https://www.simplilearn.com");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
 
